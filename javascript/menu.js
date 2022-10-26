8ff07d30ec8a1b12769af8b35d30446f5ce42dae
Antonio Aldacélio Cavalcante <82114921+Aldacelio@users.noreply.github.com>
2022-10-26 18:21:11 -0300

    inclusão do menu e plano de fundo

---
+++ javascript/menu.js
@@ -0,0 +1,24 @@
+const jogador1 = document.getElementById('jogador1');
+const jogador2 = document.getElementById('jogador2');
+const button = document.querySelector('.btn');
+const form = document.querySelector('.container-login');
+
+function validateInput(){
+    if(jogador1.value.length && jogador2.value.length ){
+        button.removeAttribute('disabled');
+    }else{
+        button.setAttribute('disabled', '');
+    }
+}
+
+const handleSubmit = (event) =>{
+    event.preventDefault();
+    localStorage.setItem('Jogador1', jogador1.value);
+    localStorage.setItem('Jogador2', jogador2.value);
+
+    window.location.assign('./Pages/game.html');
+}
+
+jogador1.addEventListener('input', validateInput);
+jogador2.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
